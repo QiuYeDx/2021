febbe38ff0455ed34564d4bd2ebc3948fa0bb469
QiuYeDx <532024989@qq.com>
2021-01-01 15:20:38 +0800

    20210101-1

---
+++ 202101/20210101-1/cmake-build-debug/CMakeFiles/20210101_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "20210101_1.exe"
+  "20210101_1.exe.manifest"
+  "20210101_1.lib"
+  "20210101_1.pdb"
+  "CMakeFiles/20210101_1.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/20210101_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
