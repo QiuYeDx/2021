0b6faeb31cb0b62f86b46261d89e7ba26f66f820
白子涵(秋夜Dx <71062076+QiuYeDx@users.noreply.github.com>
2021-01-01 18:42:26 +0800

    20210101-1

--- 202101/20210101-1/main.c
+++ 202101/20210101-1/main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    printf("test~");
+    char num1[600]={0};
+    char num2[600]={0};
+    char num[600]={0};
+    int jw[600]={0};
+    int i1,i2,i,i_max,i_min;     //i1:A的位数  i2:B的位数
+
+    for(i1=1;scanf("%c",&num1[i1]),num1[i1]!='\n';i1++)  //读入A
+        num1[i1]-=48;
+    num1[i1]='\0';
+    i1--;
+    for(i2=1;scanf("%c",&num2[i2])!=EOF;i2++)  //读入B
+        num2[i2]-=48;
+    i2--;
+    i_max=i1>i2?i1:i2;
+    i_min=i1>i2?i2:i1;
+    for(i=i_max;i>=0;i--)
+    {
+        if(i_max-i<=i_min)
+        {
+            num[i]=(num1[i]+num2[i]+jw[i])%10;
+            if(i-1>=0)
+                jw[i-1]=(num1[i]+num2[i]+jw[i])/10;
+        }
+        else
+        {
+            num[i]=num1[i]+jw[i];
+        }
+    }
+    if(num[0]!=0)
+        printf("%d",num[0]);
+    for(int k=1;k<=i_max;k++)
+        printf("%d",num[k]);
     return 0;
 }
