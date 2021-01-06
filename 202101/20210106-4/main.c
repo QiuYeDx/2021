5bdc0ae1bf013411a9ae980d8864ec0ae2620a65
QiuYeDx <532024989@qq.com>
2021-01-06 22:18:10 +0800

    20210106-4-over

--- 202101/20210106-4/main.c
+++ 202101/20210106-4/main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include <stdio.h>//输出8位生日各位数字之和
 
 int main() {
-    printf("Hello, World!\n");
+    int sum=0;
+    long num;
+
+    scanf("%ld",&num);
+    for(int i=1;i<=8;i++)
+    {
+        sum+=num%10;
+        num/=10;
+    }
+    printf("%d",sum);
     return 0;
 }
