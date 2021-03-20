9e1063e7614e9728bd9f0bc3ff1be107c29f0e7a
QiuYeDx <532024989@qq.com>
2021-03-20 10:54:16 +0800

    20210320-1

--- MySQL/20210311-1-goods_v1/20210311-1-goods_v1.sql
+++ MySQL/20210311-1-goods_v1/20210311-1-goods_v1.sql
@@ -1,1 +1,5 @@
+select * from goods_v1.goods INTO OUTFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\backup.sql';
+SET SQL_SAFE_UPDATES=0;
+delete from goods_v1.goods;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\backup.sql' INTO TABLE goods_v1.goods;
 select * from goods_v1.goods;
